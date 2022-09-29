10e784da1946eb07e43f2667b1f9eecc940642f5
俞杉麒 Yu, Shan-Chi <p78111519@gs.ncku.edu.tw>
2022-09-30 00:58:00 +0800

    Update LeetCode1313.c

--- Hw1/LeetCode1313.c
+++ Hw1/LeetCode1313.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int *decompressRLElist(int *nums, int numsSize, int *returnSize)
 {
     *returnSize = 0;
@@ -18,4 +21,43 @@ int *decompressRLElist(int *nums, int numsSize, int *returnSize)
     }
 
     return result;
+}
+
+int main()
+{
+    int size = 4, returnSize;
+
+    int *p = malloc(sizeof(int)*size);
+    *p = 1;
+    *(p+1) = 2;
+    *(p+2) = 3;
+    *(p+3) = 4;
+
+    int* result;
+    result = decompressRLElist(p,size,&returnSize);
+    for(int i=0;i<returnSize;i++)
+        printf("%d ",*(result+i));
+    printf("\n");
+
+    free(result);
+
+    size = 6;
+    p = realloc(p,sizeof(int)*size);
+
+    *p = 2;
+    *(p+1) = 2;
+    *(p+2) = 4;
+    *(p+3) = 4;
+    *(p+4) = 5;
+    *(p+5) = 5;
+
+    result = decompressRLElist(p,size,&returnSize);
+    for(int i=0;i<returnSize;i++)
+        printf("%d ",*(result+i));
+    printf("\n");
+
+    free(result);
+    free(p);
+
+    return 0;
 }
